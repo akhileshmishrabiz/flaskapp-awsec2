dfd21ba61b212de7d5731dd888dfc7a982c52de8
Akhilesh Mishra <83780705+akhileshmishrabiz@users.noreply.github.com>
2024-01-29 01:50:07 +0530

    Updated the app code to say greetings to my readers.

--- Src/app.py
+++ Src/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_docker():
-    return 'Hello, World. This is Akhilesh Mishra!!'
+    return '<h1> This is Akhilesh Mishra</h1><br><p>Thank you for reading. I hope you enjoyed it, follow for more content around Devops.</p> '
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
